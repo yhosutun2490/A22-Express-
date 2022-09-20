424017b458063d9e6e13ced23adbf50f3ec2ab38
yhosutun2491 <yhosutun2491@gmail.com>
2022-09-20 16:40:23 +0800

    set handlebars and layouts  and static files

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const port = 3000
 // 樣板引擎基本設定
 app.engine('handlebars', exphbs({ defaultLayout: 'main' })) //預設顯示的主模板名稱
 app.set('view engine', 'handlebars') //如果沒設定樣板引擎，預設是pug
+// 建立靜態資料夾路徑
+app.use(express.static('public'))
 //設定相關路由
 app.get('/', (req, res) => {
   res.send('HI I AM Rafael')
