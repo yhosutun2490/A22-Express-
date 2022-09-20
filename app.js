d30962cf11b3a7d69f9cef6e72e7b041c3871ff6
yhosutun2491 <yhosutun2491@gmail.com>
2022-09-20 20:55:32 +0800

    JS standard check

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//導入express 框架
+// 導入express 框架
 const express = require('express')
 const app = express()
 // 導入樣板引擎
@@ -7,11 +7,11 @@ const exphbs = require('express-handlebars')
 const hostname = 'localhost'
 const port = 3000
 // 樣板引擎基本設定
-app.engine('handlebars', exphbs({ defaultLayout: 'main' })) //預設顯示的主模板名稱
-app.set('view engine', 'handlebars') //如果沒設定樣板引擎，預設是pug
+app.engine('handlebars', exphbs({ defaultLayout: 'main' })) // 預設顯示的主模板名稱
+app.set('view engine', 'handlebars') // 如果沒設定樣板引擎，預設是pug
 // 建立靜態資料夾路徑
 app.use(express.static('public'))
-//設定相關路由
+// 設定相關路由
 const pointer = 'b-black'
 app.get('/', (req, res) => {
   res.render('index')
@@ -26,7 +26,7 @@ app.get('/portfolio', (req, res) => {
 app.get('/contact', (req, res) => {
   res.render('contact', { clickcontact: pointer })
 })
-//監聽伺服器
+// 監聽伺服器
 app.listen(3000, () => {
   console.log(`you are listening on ${hostname} en port ${port}`)
 })
